ef7173b39ae30cb5c8ded57451bc44bafcfdc410
Sachin Pal <sachinpal@farmakart.com>
2026-02-03 16:01:04 +0530

    allow cors origin

--- src/app.js
+++ src/app.js
@@ -19,7 +19,8 @@ const corsOptions = {
             'http://localhost:3000',
             'http://localhost:4000',
             'http://localhost:8080',
-            'http://127.0.0.1:8080'
+            'http://127.0.0.1:8080',
+            'https://missioncontrol.myrsolar.com/'
             // Add your production frontend URL here when deployed
             // 'https://your-frontend-domain.com'
         ];
